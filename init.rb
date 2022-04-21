6fe76ecca76c1c6506b6106b350ee2738247cff9
theirix <theirix@gmail.com>
2022-04-21 12:44:32 +0300

    Bump version

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_issuepoke do
   author 'Eugene Seliverstov'
   author_url 'http://omniverse.ru'
   description 'IssuePoke plugin'
-  version '0.0.6'
+  version '0.0.7'
   url 'http://github.com/theirix/redmine_issuepoke'
 
   require 'redmine_issuepoke'
